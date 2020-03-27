1cc20dc5e3257dd9e82b4fa8d474f12df05518cf
sheetal2211 <36146595+sheetal2211@users.noreply.github.com>
2020-03-27 20:43:59 +0530

    feat(dashboard): dashboard module as angular elements (#274)
    * feat(dashboard): basic dashboard micro app created
    
    * feat(dashboard): integrated dashboard module with main app
    
    * feat(dashboard-elements): building dashboard module as angular elements
    
    * feat(dashboard-elements): dashboard elements build file added
    
    * fix(dashboard): fix lint issues
    
    * fix(dashboard): fix issues reported by travis
    
    * fix(dashboard): using chrome headless

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from '../../projects/ff4j-features/src/app/features.module';
 import { PropertiesModule } from '../../projects/ff4j-properties/src/app/properties.module';
+import { DashboardModule } from '../../projects/ff4j-dashboard/src/app/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FeaturesModule, PropertiesModule],
+  imports: [BrowserModule, AppRoutingModule, FeaturesModule, PropertiesModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
