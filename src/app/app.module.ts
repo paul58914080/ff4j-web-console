dc2dcc6d3f4a198f916b4966f8209ab9fa8f8080
paul58914080 <paul58914080@gmail.com>
2020-03-28 11:44:36 +0530

    refactor: renamed ff4j-dashboards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FeaturesModule } from '../../projects/ff4j-features/src/app/features.module';
-import { PropertiesModule } from '../../projects/ff4j-properties/src/app/properties.module';
-import { DashboardModule } from '../../projects/ff4j-dashboard/src/app/dashboard.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FeaturesModule } from "../../projects/ff4j-features/src/app/features.module";
+import { PropertiesModule } from "../../projects/ff4j-properties/src/app/properties.module";
+import { DashboardModule } from "../../projects/ff4j-dashboards/src/app/dashboard.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FeaturesModule, PropertiesModule, DashboardModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FeaturesModule,
+    PropertiesModule,
+    DashboardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
