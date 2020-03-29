b232b13e6a04274145565959efe9dfabf6deb96d
Paul Williams <paul58914080@gmail.com>
2020-03-29 21:22:15 +0530

    refactor(dashboard): enhancements for micro-frontend (#289)
    * feat(ff4-dashboards): scripts to run, test and lint dashboards added
    
    * feat(ff4-dashboards): enabling code coverage for dashboards sub project
    
    * feat(ff4-dashboards): lazy loading for dashboards module
    
    * feat(ff4-dashboards): dashboard browser module added for hosting browsers sub project independently
    
    * feat(style): using global styles in sub projects
    
    * test(ff4j-dashboards): e2e tests corrected
    
    * chore: updated templates, CODEOWNERS and CONTRIBUTING
    
    * build: updated the path of the elements builder
    
    * ci: updated the path ghPages
    
    * feat(ff4-dashboards): scripts to run, test and lint dashboards added
    
    * build: updated the styles path of micro-frontend
    
    * docs: updated badges and information
    
    * docs: updated pre-requisite section
    
    * refactor: removed styles.scss
    
    * build: renamed elements builder for micro-frontend
    
    Co-authored-by: sheetal2211 <sheetal.agarwal2211@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     path: 'properties',
     loadChildren: () => import('../../projects/ff4j-properties/src/app/properties.module').then((m) => m.PropertiesModule),
   },
+  {
+    path: 'dashboards',
+    loadChildren: () => import('../../projects/ff4j-dashboards/src/app/dashboard.module').then((m) => m.DashboardModule),
+  },
   { path: '', redirectTo: '/features', pathMatch: 'full' },
   { path: '**', redirectTo: 'features' },
 ];
