c3895e734020a42fa53addfe84f138bceb134337
Paul <paul58914080@gmail.com>
2020-07-12 23:37:34 +0530

    style: added material theme and customized it to ff4j

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertiesModule } from '../../projects/ff4j-properties/src/app/properties.module';
 import { DashboardModule } from '../../projects/ff4j-dashboards/src/app/dashboard.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PropertiesModule, DashboardModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PropertiesModule,
+    DashboardModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
