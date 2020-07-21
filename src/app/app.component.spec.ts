d20fed260ec87eff0a9140350ca7aa5c8a234830
Paul Williams <paul58914080@gmail.com>
2020-07-21 22:05:48 +0530

    feat: toggle light and dark theme (#420)
    * feat(theme): add dark and light theme switch
    
    * refactor: removed ThemeService and changed the styles for dark and light theme
    
    Co-authored-by: Anup Kumar <anup.raj1989@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,37 @@
-import {async, TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {AppComponent} from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 describe('AppComponent', () => {
+  let fixture;
+  let component;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        MatToolbarModule,
-        MatIconModule
-      ],
-      declarations: [AppComponent]
+      imports: [RouterTestingModule, MatToolbarModule, MatIconModule, MatSlideToggleModule, MatIconModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+    expect(component.isLightTheme).toBeFalse();
+  });
+
+  it('should be able to toggle theme', () => {
+    component.toggleTheme(true);
+    expect(component.isLightTheme).toBeTrue();
+    component.toggleTheme(false);
+    expect(component.isLightTheme).toBeFalse();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ff4j-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLightTheme = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.isLightTheme = false;
+  }
+
+  toggleTheme(checked: boolean): void {
+    this.isLightTheme = checked;
+  }
 }
